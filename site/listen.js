36de8464333130471d5dde58c47c93168b53a168
Carlos Rodriguez <carlos@s8f.org>
2016-04-30 00:06:53 -0700

    tweaks

--- site/listen.js
+++ site/listen.js
@@ -1,28 +1,24 @@
 var cluster = require('cluster')
 
 module.exports = function container (get) {
-  return function alter (_listen) {
+  return function alter (listen) {
     return function task (cb) {
       var options = get('conf.site.cluster')
 
       if (cluster.isMaster) {
         if (options.workers === 'auto') options.workers = require('os').cpus().length
-        cluster.on('disconnect', function (worker) {
-          console.error('motley-cluster: worker ' + worker.process.pid + ' disconnected. killing...')
-          worker.kill()
-        })
         cluster.on('exit', function (worker, code, signal) {
           var exitCode = worker.process.exitCode
           if (worker.exitedAfterDisconnect) {
-            console.error('motley-cluster: worker ' + worker.process.pid + ' exited after disconnect.')
+            console.error('motley-cluster: worker ' + worker.id + ' exited after disconnect.')
           }
           else if (options.respawn) {
-            console.error('worker ' + worker.process.pid + ' died (' + exitCode + '). respawning...')
+            console.error('motley-cluster: worker ' + worker.id + ' died (' + exitCode + '). respawning...')
             cluster.fork()
           }
           else {
-            console.error('worker ' + worker.process.pid + ' died (' + exitCode + '). crashing...')
-            process.stderr.once('drain', function () {
+            console.error('motley-cluster: worker ' + worker.id + ' died (' + exitCode + '). crashing...')
+            cluster.disconnect(function () {
               process.exit(1)
             })
           }
@@ -32,9 +28,7 @@ module.exports = function container (get) {
         }
         setImmediate(cb)
       }
-      else {
-        cb()
-      }
+      else listen(cb)
     }
   }
 }
