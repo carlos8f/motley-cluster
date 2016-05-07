09f919012585688dee04e041ab28ea8ed1f763eb
Carlos Rodriguez <carlos@s8f.org>
2016-05-06 19:54:19 -0700

    use new console

--- site/listen.js
+++ site/listen.js
@@ -9,15 +9,15 @@ module.exports = function container (get) {
         if (options.workers === 'auto') options.workers = require('os').cpus().length
         cluster.on('exit', function (worker, code, signal) {
           var exitCode = worker.process.exitCode
-          if (worker.exitedAfterDisconnect) {
-            console.error('motley-cluster: worker ' + worker.id + ' exited after disconnect.')
+          if (!exitCode) {
+            get('console').error('motley-cluster: worker ' + worker.id + ' exited (' + exitCode + ').')
           }
           else if (options.respawn) {
-            console.error('motley-cluster: worker ' + worker.id + ' died (' + exitCode + '). respawning...')
+            get('console').error('motley-cluster: worker ' + worker.id + ' crashed (' + exitCode + '). respawning...')
             cluster.fork()
           }
           else {
-            console.error('motley-cluster: worker ' + worker.id + ' died (' + exitCode + '). crashing...')
+            get('console').error('motley-cluster: worker ' + worker.id + ' crashed (' + exitCode + '). crashing...')
             cluster.disconnect(function () {
               process.exit(1)
             })
